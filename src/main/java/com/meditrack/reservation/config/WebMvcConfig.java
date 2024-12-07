873b59dd6a92af2d49e71bab34c46750ef7d76f1
RatnasriNilakshan <nilakslin@gmail.com>
2024-12-07 19:10:59 +0300

    K8s config add

--- src/main/java/com/meditrack/reservation/config/WebMvcConfig.java
+++ src/main/java/com/meditrack/reservation/config/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package com.meditrack.reservation.config;
 
 import com.meditrack.reservation.interceptor.HeaderInterceptor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,10 +9,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
+    @Value("${spring.datasource.url}")
+    private String datasourceUrl;
+
     private final HeaderInterceptor headerInterceptor;
 
     public WebMvcConfig(HeaderInterceptor headerInterceptor) {
         this.headerInterceptor = headerInterceptor;
+        System.out.println("Datasource URL: " + datasourceUrl);
     }
 
     @Override
