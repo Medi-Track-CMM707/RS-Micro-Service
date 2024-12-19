bca0dd68d613a17dd9591c86c1c886ac29e57782
RatnasriNilakshan <nilakslin@gmail.com>
2024-12-19 06:54:50 +0530

    testfile

--- src/test/java/com/meditrack/reservation/ReservationApplicationTests.java
+++ src/test/java/com/meditrack/reservation/ReservationApplicationTests.java
@@ -4,10 +4,17 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application.yml")
 class ReservationApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testDatabaseConnection() {
+		assertDoesNotThrow(() -> {
+			// Ensure datasource is autowired and usable
+			DataSource dataSource = applicationContext.getBean(DataSource.class);
+			try (Connection connection = dataSource.getConnection()) {
+				assertNotNull(connection);
+			}
+		});
 	}
-
 }
